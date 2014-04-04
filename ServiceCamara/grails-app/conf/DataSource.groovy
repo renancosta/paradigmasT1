ddba5a93587b537e44bc605bacf15afd0fd63783
Renan Costa <renan2727@hotmail.com>
2014-04-04 11:40:28 -0300

    Persistência adicionada

--- ServiceCamara/grails-app/conf/DataSource.groovy
+++ ServiceCamara/grails-app/conf/DataSource.groovy
@@ -1,12 +1,11 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+    driverClassName = "com.mysql.jdbc.Driver"
+    dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
 }
 hibernate {
     cache.use_second_level_cache = true
-    cache.use_query_cache = false
+    cache.use_query_cache = true
     cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
 }
 // environment specific settings
@@ -14,30 +13,25 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:mysql://localhost/camara?useUnicode=yes&characterEncoding=UTF-8"
+            username = "root"
+            password = "mysqlmysql"
         }
     }
     test {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+          dbCreate = "update"
+            url = "jdbc:mysql://localhost/camara?useUnicode=yes&characterEncoding=UTF-8"
+            username = "root"
+            password = "mysqlmysql"
         }
     }
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
+            url = "jdbc:mysql://localhost/camara_prod?useUnicode=yes&characterEncoding=UTF-8"
+            username = "root"
+            password = "mysqlmysql"
         }
     }
 }
